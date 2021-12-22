21b8225b890a58407695dac2425b5cf9c9b843ad
Yiqun Zhang <875977494@qq.com>
2021-12-22 16:39:59 +0800

    修改信息

--- UserInformation.py
+++ UserInformation.py
@@ -9,6 +9,6 @@ u0 = User('新用户', '', '111111', '')
 u1 = User('Admin', '875977494@qq.com', 'darkspiderman', 'enuhxtcuorumbbeg')
 u2 = User('张媛芳', '2766366959@qq.com', '123581321zyf20011022', 'syzdmhtsfdlkdgdc')
 u3 = User('韩睿', '303683509@qq.com', '123456', 'pztrvgkquisxbhgj')
-u4 = User('么保鑫', '408456762@qq.com', '123456', 'wxrxjpxiwefkbgeh')
+u4 = User('段尊霖', '408456762@qq.com', '123456', 'wxrxjpxiwefkbgeh')
 u5 = User('杨晗旖', '869804595@qq.com', '123456', 'smhqxqlgndvpbedh')
 users = [u0, u1, u2, u3, u4, u5]
